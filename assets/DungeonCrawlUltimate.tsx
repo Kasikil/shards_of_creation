14825dc62c4d7cf0186a3a93dbb51e98e41916c1
Clayt <clayton.wwjd@gmail.com>
2020-07-29 21:58:12 -0500

    Updated to a custom tileset

--- assets/DungeonCrawlUltimate.tsx
+++ assets/DungeonCrawlUltimate.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.1" name="DungeonCrawlUltimate" tilewidth="32" tileheight="32" tilecount="3072" columns="64">
- <image source="../img/DungeonCrawl_ProjectUtumnoTileset.png" width="2048" height="1536"/>
+<tileset version="1.4" tiledversion="1.4.1" name="DungeonCrawlUltimate" tilewidth="32" tileheight="32" tilecount="900" columns="30">
+ <image source="../img/ShardsOfCreationTileset.png" width="960" height="960"/>
 </tileset>
